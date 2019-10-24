69d37ec6ce6fc5b5bac17185ed9f4bd1072e5e7f
zmeriksen <zmeriksen@gmail.com>
2019-10-23 19:23:11 -0500

    Initial Commit

---
+++ BlackJack/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BlackJack
+//
+//  Created by Zach Eriksen on 10/23/19.
+//  Copyright © 2019 oneleif. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
